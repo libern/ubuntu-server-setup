af6734efcfb82ffeea859b13ce20178848e2e9c6
Libern <libernlin@gmail.com>
2016-06-02 22:55:20 +0800

    Improved scripts

--- bash-scripts/14.04/install_essentials.sh
+++ bash-scripts/14.04/install_essentials.sh
@@ -3,7 +3,7 @@
 
 # make sure system is up to date
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
 # install essentials
 sudo apt-get install -y htop nethogs iptraf pydf zip nmap fail2ban glances git
