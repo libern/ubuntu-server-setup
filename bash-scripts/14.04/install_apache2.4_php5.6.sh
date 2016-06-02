7a6080be1be14a9a85b68b7e1562d2900ca50d93
Libern <libernlin@gmail.com>
2016-06-02 23:32:42 +0800

    Improved scripts

--- bash-scripts/14.04/install_apache2.4_php5.6.sh
+++ bash-scripts/14.04/install_apache2.4_php5.6.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
-echo "=============== Installing Apache 2.4 + PHP 5.5 =================="
+echo "=============== Installing Apache 2.4 + PHP 5.6 =================="
 echo -n "Press ENTER to continue install: "
 read confirm
 
 sudo apt-get install -y python-software-properties
-sudo add-apt-repository -y ppa:ondrej/php5
+sudo add-apt-repository -y ppa:ondrej/php
 
 sudo apt-get update
 
 # install essentials
-sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-mysql php5-gd php5-mcrypt
+sudo apt-get install -y php5.6 apache2 libapache2-mod-php5 php5-mysql php5-gd php5-mcrypt
 
 sudo a2enmod rewrite
 
