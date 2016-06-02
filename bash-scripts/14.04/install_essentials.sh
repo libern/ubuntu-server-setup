8fae101bb5309003da9fd97bcb1326812234e5db
Libern <libernlin@gmail.com>
2016-06-02 22:51:51 +0800

    Added new scripts

---
+++ bash-scripts/14.04/install_essentials.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+
+# make sure system is up to date
+sudo apt-get update
+sudo apt-get upgrade
+
+# install essentials
+sudo apt-get install -y htop nethogs iptraf pydf zip nmap fail2ban glances git
+
+echo "Done."
---
+++ bash-scripts/14.04/install_lamp.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+echo "\n=============== Installing Apache + PHP + MySQL ==================\n"
+echo -n "Press ENTER to continue install: "
+read confirm
+
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository -y ppa:ondrej/php5
+
+sudo apt-get update
+
+# install essentials
+#sudo apt-get install -y htop nethogs iptraf pydf zip nmap fail2ban glances git
+
+sudo apt-get install -y php5 apache2 libapache2-mod-php5 mysql-server php5-mysql php5-gd php5-mcrypt mysql-client php5-curl php5-mbstring
+
+sudo a2enmod rewrite
+
+#sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/sites-available/phpmyadmin.conf
+#sudo a2ensite phpmyadmin
+
+sudo service apache2 reload
+
+echo "Done."
