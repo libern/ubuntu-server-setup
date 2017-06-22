82f998a98b3ba20e670ea6b2616aa086cbc7286f
Libern <libernlin@gmail.com>
2017-06-22 13:49:26 +0800

    Improved backup

--- bash-scripts/database/backup.sh
+++ bash-scripts/database/backup.sh
@@ -15,7 +15,7 @@ mkdir -p "$ALL_DIR"
 databases=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
  
 for db in $databases; do
-  $MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | gzip > "$BACKUP_DIR/$db.gz"
+  $MYSQLDUMP --force --opt --default-character-set=utf8mb4 --complete-insert --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | gzip > "$BACKUP_DIR/$db.gz"
 done
 
 echo "Compressing..."
