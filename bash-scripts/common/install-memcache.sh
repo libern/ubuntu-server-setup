d3181164ba43e97561ecb052551d0522cfc41ede
Libern <libernlin@gmail.com>
2018-01-08 12:47:42 +0800

    Improved

---
+++ bash-scripts/common/install-memcache.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-in-ubuntu-16-04
+
+#
+# UPDATE / UPGRADE
+#
+
+echo "--------------- Update";
+
+sudo apt-get update;
+#sudo apt-get -y upgrade;
+
+#
+#  INSTALL
+#
+
+echo "--------------- Installing Redis";
+
+# Redis
+sudo apt-get install -y php-memcache php-memcached
