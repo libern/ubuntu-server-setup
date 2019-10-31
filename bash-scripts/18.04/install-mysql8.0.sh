e0ea8555c985c5f30970faa0e8ae3dc5058ee1a6
Libern <libernlin@gmail.com>
2019-10-31 11:53:25 +0800

    Added install-mysql8.0.sh

---
+++ bash-scripts/18.04/install-mysql8.0.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-the-latest-mysql-on-ubuntu-18-04
+
+
+echo "--------------- Config MySQL Opt";
+
+cd /tmp
+curl -OL https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
+sudo dpkg -i mysql-apt-config*
+
+#
+# UPDATE / UPGRADE
+#
+
+echo "--------------- Update";
+
+sudo apt update;
+#sudo apt-get -y upgrade;
+
+rm /tmp/mysql-apt-config*
+
+#
+#  INSTALL
+#
+
+echo "--------------- Installing MySQL";
+
+# MySQL 8.0
+sudo apt-get install -y mysql-server
+
+systemctl status mysql
+
+sudo mysql_secure_installation
+
