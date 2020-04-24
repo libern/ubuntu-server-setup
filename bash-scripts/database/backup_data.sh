05887e2862cc1748cb8549b7ab0972a96ef16f64
Libern <libernlin@gmail.com>
2020-04-24 11:07:52 +0800

    Improved dump timezone

--- bash-scripts/database/backup_data.sh
+++ bash-scripts/database/backup_data.sh
@@ -8,14 +8,14 @@ BACKUP_DIR="/srv/db/$TIMESTAMP"
 ALL_DIR="/srv/db/all"
 MYSQL=/usr/bin/mysql
 MYSQLDUMP=/usr/bin/mysqldump
- 
+
 mkdir -p "$BACKUP_DIR"
 mkdir -p "$ALL_DIR"
- 
+
 databases=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
- 
+
 for db in $databases; do
-  $MYSQLDUMP --force --opt --default-character-set=utf8mb4 --complete-insert --no-create-info --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | gzip > "$BACKUP_DIR/$db.gz"
+  $MYSQLDUMP --force --opt --default-character-set=utf8mb4 --complete-insert --no-create-info --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db --skip-tz-utc | gzip > "$BACKUP_DIR/$db.gz"
 done
 
 echo "Compressing..."
